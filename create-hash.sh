3995f9eb3c9a561bb0c0c6414527f577c9ae9e15
void daek <voiddaek@gmail.com>
2022-07-25 14:16:35 +0200

    adding information in the hash file

--- create-hash.sh
+++ create-hash.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+echo -e "hash were generated using:" >> /usr/share/nginx/html/hash.txt
+echo -e "sha256sum <filepath>" >> /usr/share/nginx/html/hash.txt
+echo -e "" >> /usr/share/nginx/html/hash.txt
+
 for f in /usr/share/nginx/html/*.*; 
 do
     text=$(sha256sum $f)
